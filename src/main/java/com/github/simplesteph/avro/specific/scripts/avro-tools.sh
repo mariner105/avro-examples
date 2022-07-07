c7360d8967aeb35322cd7a4eeec40302a074c33f
Frank Hundley <mariner105@msn.com>
2022-07-07 11:33:53 -0700

    Fixed URL for avro-tools jar

--- src/main/java/com/github/simplesteph/avro/specific/scripts/avro-tools.sh
+++ src/main/java/com/github/simplesteph/avro/specific/scripts/avro-tools.sh
@@ -1,4 +1,4 @@
-wget http://central.maven.org/maven2/org/apache/avro/avro-tools/1.11.0/avro-tools-1.11.0.jar
+wget https://repo1.maven.org/maven2/org/apache/avro/avro-tools/1.11.0/avro-tools-1.11.0.jar
 
 # run this from our project folder.  Make sure avro-tools-1.11.0.jar is your acual avro tools jar
 java -jar avro-tools-1.11.0.jar tojson --pretty customer-specific.avro
